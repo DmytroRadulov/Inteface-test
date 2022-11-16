fa212fe64b1a7ede3bf4d7d0bf3b828af50c4e16
DmytroRadulov <radulovdima1995@gmail.com>
2022-11-16 16:07:16 +0200

    init 1

---
+++ src/UserAgentInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Web\UserAgent\Test;
+
+interface UserAgentInterface
+{
+    public function setUserAgent($userAgent = null): ?string;
+
+    public function getBrowser(): ?string;
+
+    public function getPlatform(): ?string;
+}
